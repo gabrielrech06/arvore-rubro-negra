bf4d2c085f3114d61bf6220e6389c766aa26343b
Gabriel Rech <ggralowrech@gmail.com>
2025-11-05 11:42:42 -0300

    refactor: exportar tipo Cor e não exportar criação/inserção/remoção de nós

--- arvore-rn.h
+++ arvore-rn.h
@@ -1,5 +1,5 @@
-#ifndef __ARVORE_RN__
-#define __ARVORE_RN__
+#ifndef _ARVORE_RN_
+#define _ARVORE_RN_
 
 // Árvore Rubro-Negra
 //
@@ -11,6 +11,8 @@
 //// --- tipos exportados ---
 typedef struct no No;
 typedef struct arvore Arvore;
+typedef enum { VERMELHO, PRETO } Cor;
+
 
 
 //// --- criação / destruição ---
@@ -20,16 +22,21 @@ typedef struct arvore Arvore;
 // quem chamar deve liberar com arv_libera_arvore quando não for mais útil.
 Arvore* arv_cria();
 
-// cria e retorna um nó com o valor de `v`.
-// retorna um ponteiro para novo nó ou NULL em caso de falha de alocação.
-// quem chamar deve liberar com `arv_libera_arvore` ou `arv_libera_no` quando não for mais útil.
-No* arv_cria_no(int v);
-
 // libera toda a árvore rubro-negra.
 void arv_libera_arvore(Arvore *arv);
 
-// libera o nó e suas subárvores.
-void arv_libera_no(No *no);
+
+
+//// --- inserção/remoção ---
+
+// insere na árvore rubro-negra o nó com o valor passado como argumento.
+// retorna true se for bem sucedido ou false caso não.
+bool arv_insere_no(Arvore *arv, int v);
+
+// remove da árvore rubro-negra o nó com o valor passado como argumento.
+// retorna true se for bem sucedido ou false caso não.
+bool arv_remove_no(Arvore *arv, int v);
+
 
 
 //// --- consultas ---
@@ -42,26 +49,28 @@ bool arv_vazia(Arvore *arvore);
 bool arv_no_vazio(No *no);
 
 // retorna um ponteiro para o nó raiz da árvore
-No* arv_busca_raiz(Arvore *arvore);
+No* arv_busca_raiz(Arvore *arv);
 
 // retorna um ponteiro para o filho do nó.
 // `dir` == false -> filho esquerdo.
 // `dir` == true -> filho direito.
 No* arv_busca_filho(No *no, bool dir);
 
+// retorna a cor do nó.
+Cor arv_busca_cor(No *no);
+
 // retorna um ponteiro para o nó pai.
 No* arv_busca_pai(No *no);
 
-// retorna o valor do dado do nó.
-// retorna true e grava em `*retorno` o valor se no != NULL.
-// retorna false se no == NULL.
+// retorna true e grava em `*retorno` o valor do nó.
+// retorna false se `no` == NULL ou `retorno` == NULL.
 bool arv_busca_valor(No *no, int *retorno);
 
 // retorna um ponteiro para o nó com o valor buscado.
 // procura a partir de `raiz`.
 No* arv_busca_no(No *raiz, int v);
 
-// retorna true se a árvore conter o valor ou false senão conter.
+// retorna true se a árvore conter o valor `v` ou false senão conter.
 bool arv_contem(Arvore *arv, int v);
 
 // retorna a altura da árvore.
@@ -72,17 +81,6 @@ No* arv_busca_minimo(No *raiz);
 
 // retorna um ponteiro para o nó com maior valor a partir do nó passado como argumento.
 No* arv_busca_maximo(No *raiz);
-
-
-//// --- inserção/remoção ---
-
-// insere na árvore rubro-negra o nó com o valor passado como argumento.
-// retorna true se for bem sucedido ou false caso não.
-bool arv_insere_no(Arvore *arv, int v);
-
-// remove da árvore rubro-negra o nó com o valor passado como argumento.
-// retorna true se for bem sucedido ou false caso não.
-bool arv_remove_no(Arvore *arv, int v);
 
 
 
